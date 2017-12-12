084f1a2ef112b97ebc16ae955c90ff3a1ef0a0fe
Abhihsek Sudhakaran <itzurabhi@gmail.com>
2017-12-12 15:05:57 +0530

    Print 50 Centers

--- print.js
+++ print.js
@@ -3,6 +3,8 @@ app.controller('PrintController', function($scope) {
     $scope.TotalPageCount = 10;
     $scope.Pages = Array($scope.TotalPageCount);
     $scope.ItemsPerPage = 50;
+
+    $scope.PageItems = Array($scope.ItemsPerPage);
     $scope.Items = [{
         NumberCol: 0,
         TitleCol: "Title Even",
